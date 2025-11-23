7d02c7ce9d71e48dd8b011ef7930457b8c0c708c
therobinplays <therealrobinplys>
2025-11-23 15:18:58 +0545

    natural number code

---
+++ Natural.js
@@ -0,0 +1,10 @@
+ function sum(n) {
+     let sum = 0;
+        for (let i = 0; i <= n; i++) {
+            sum += i;
+        }
+       return sum;
+
+    }
+
+   console.log("The sum of n natural number is",sum(5));
