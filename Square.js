ff12da838291e3e07bf673877067d1c3fab67154
therobinplays <therealrobinplys>
2025-12-03 11:18:20 +0545

    add

---
+++ Square.js
@@ -0,0 +1,4 @@
+const numbers = [1,2,3,4,5];
+const squaredNumbers = numbers.map(function (num)) {
+    return num*num;
+})
---
+++ map.js
@@ -0,0 +1,5 @@
+const intro = ["SuSan","Sanam","Saugat","Samyog"];
+
+const caps = intro.map(intro =>intro.toUpperCase());
+
+console.log(caps);
